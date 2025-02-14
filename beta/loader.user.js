0c9531392ee4a9d140d7b85c4bd787be39f4ab32
zoe-64 <118383959+zoe-64@users.noreply.github.com>
2025-02-14 12:11:40 +0100

    fixed minigames not working in beta

--- beta/loader.user.js
+++ beta/loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name ABCL (Loader)
 // @namespace https://www.bondageprojects.com/
-// @version 1.6
+// @version 2.0.0-beta
 // @description An addon for [Bondage Club](https://www.bondageprojects.com/club_game/). Stands for "Adult baby club lover"~
 // @author Zoe, Maple, En
 // @match https://*.bondageprojects.elementfx.com/R*/*
