86349f07239f935746ac60d8617443d82d94da17
zoe-64 <118383959+zoe-64@users.noreply.github.com>
2025-02-14 11:44:24 +0100

    URL fix

--- beta/loader.user.js
+++ beta/loader.user.js
@@ -8,14 +8,14 @@
 // @match https://*.bondage-europe.com/R*/*
 // @match https://*.bondageprojects.com/R*/*
 // @match http://localhost:*/*
-// @icon  https://github.com/zoe-64/ABCL/raw/testing-v2/beta/assets/favicon.ico
+// @icon  https://zoe-64.github.io/ABCL/beta/assets/favicon.ico
 // @grant none
 // @run-at document-end
 // ==/UserScript==
 
 (function () {
   "use strict";
-  const src = `https://github.com/zoe-64/ABCL/raw/testing-v2/beta/main.js?v=${Date.now()}`;
+  const src = `https://zoe-64.github.io/ABCL/beta/main.js?v=${Date.now()}`;
   if (typeof ABCL_Loaded === "undefined") {
     const script = document.createElement("script");
     script.src = src;
