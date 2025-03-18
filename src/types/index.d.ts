9105c93974b318b0a3a9485b11d8d8fee5725027
zoe-64 <118383959+zoe-64@users.noreply.github.com>
2025-03-18 18:09:25 +0100

    littlish typing

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,25 +1,6 @@
 interface Window {
   modLoadFlag?: boolean;
-  LITTLISH_CLUB: {
-    inModSubscreen(): boolean;
-    getCaregiversOf(C: Character): number[];
-    getMommyOf(C: Character): {
-      name: string;
-      id: number;
-    } | null;
-    hasAccessRightTo(C1: Character, C2: Character, accessRight: AccessRight): boolean;
-  };
-}
-// Littlish club
-declare enum AccessRight {
-  MANAGE_DIAPER = "MANAGE_DIAPER",
-  MANAGE_RULES = "MANAGE_RULES",
-  TURN_RULE_STRICT_MODE = "TURN_RULE_STRICT_MODE",
-  DELETE_NOTES = "DELETE_NOTES",
-  MANAGE_APPEARANCE = "MANAGE_APPEARANCE",
-  MANAGE_CAREGIVERS_ACCESS_RIGHTS = "MANAGE_CAREGIVERS_ACCESS_RIGHTS",
-  TURN_PREVENT_BABY_FROM_CHANGING_CAREGIVERS_LIST = "TURN_PREVENT_BABY_FROM_CHANGING_CAREGIVERS_LIST",
-  CHANGE_CAREGIVERS_LIST = "CHANGE_CAREGIVERS_LIST",
+  LITTLISH_CLUB: LITTLISH_CLUB_API;
 }
 
 declare const modVersion: string;
