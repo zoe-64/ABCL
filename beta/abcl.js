555c85d5c4bbdd9d0cf9ecd2ba3f8b91896805d9
zoe-64 <118383959+zoe-64@users.noreply.github.com>
2025-03-20 10:56:43 +0100

    Update abcl.js

--- beta/abcl.js
+++ beta/abcl.js
@@ -4,7 +4,7 @@
 		<h1> Outdated Beta </h1>
 		<p> You are using an outdated beta version of ABCL. Please switch to the latest version to ensure compatibility and security. </p>
 		<a href="https://github.com/zoe-64/ABCL/blob/main/beta/bookmark.js"> Bookmark </a></br>
-		<a href="https://github.com/zoe-64/ABCL/raw/main/beta/loader.user.js"> Monkey Loader </a>
+		<a href="https://github.com/zoe-64/ABCL/raw/main/versions/beta/loader.user.js"> Monkey Loader </a>
 		</br><button id="closeButton" style="position:absolute;bottom:15px;right:15px"> Close </button>
 		</div>`;
   document.body.appendChild(dialog);
